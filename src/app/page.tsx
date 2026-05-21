76f9d91e3425e58dc30a5e4f21e6d7b2158f0302
Tarun <tarunsingh95409@gmail.con>
2026-05-21 21:26:56 +0530

    save before merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,3 @@ export default function Home() {
   )
 }
 //shadcn@4.7.0
-
-postgresql://neondb_owner:npg_sVK7TeSx3fhN@ep-dark-queen-aq7fkzmx.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require
-
-// npx neonctl@latest init
-
-postgresql://neondb_owner:npg_sVK7TeSx3fhN@ep-dark-queen-aq7fkzmx-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
