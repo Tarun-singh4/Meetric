ceb23da2e25bf7f9a9aa1b9aac9f49e2e1a0bc40
Tarun <tarunsingh95409@gmail.con>
2026-06-25 01:01:33 +0530

    13:Agents-filters

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,7 +3,8 @@ import {db} from "@/src/db";
 import {agents} from "@/src/db/schema";
 import { agentsInsetSchema } from "../schemas";
 import {z} from "zod";
-import {eq,getTableColumns,sql} from "drizzle-orm";
+import {eq,getTableColumns,sql,and,ilike,desc,count} from "drizzle-orm";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/src/constants";
 
 export const agentsRouter=createTRPCRouter({
     getOne:protectedProcedure
@@ -18,10 +19,51 @@ export const agentsRouter=createTRPCRouter({
         return existingAgent;
     }),
 
-    getMany:protectedProcedure.query(async()=>{
-        const data =await db.select().from(agents);
+    getMany:protectedProcedure
+    .input(z.object({
+         page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish()
+    }))
+    .query(async({ctx,input})=>{
+        const { search, page, pageSize } = input;
 
-        return data;
+        const data =await db.select({
+             ...getTableColumns(agents),
+            meetingCount:sql<number>`5`,
+        }).from(agents)
+        .where(
+            and(
+                eq(agents.userId,ctx.auth.user.id),
+                search ? ilike(agents.name, `%${search}%`) : undefined,
+            )
+        )
+         .orderBy(desc(agents.createdAt), desc(agents.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize)
+
+        const [total] = await db
+        .select({ count: count() })
+        .from(agents)
+        .where(
+          and(
+            eq(agents.userId, ctx.auth.user.id),
+            search ? ilike(agents.name, `%${search}%`) : undefined,
+          )
+        );
+
+        const totalPages = Math.ceil(total.count / pageSize);
+
+
+        return {
+        items: data,
+        total: total.count,
+        totalPages,
+      };
     }),
     create:protectedProcedure
     .input(agentsInsetSchema)
