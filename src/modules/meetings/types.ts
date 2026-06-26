72e90b3a0ca2d7e43639bd8c41645074382fcec1
Tarun <tarunsingh95409@gmail.con>
2026-06-26 12:05:26 +0530

    18:meetings-data-table

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -4,3 +4,5 @@ import { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/src/trpc/routers/_app";
 
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
+
+export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
